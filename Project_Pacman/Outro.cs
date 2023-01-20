c7618b01abb31b7646c0d6150e8b2ef0edb28477
Jin <122707377+intrigued07@users.noreply.github.com>
2023-01-20 11:59:26 +0900

    Update Outro.cs

--- Project_Pacman/Outro.cs
+++ Project_Pacman/Outro.cs
@@ -1,44 +1,44 @@
-﻿using PacMan;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Project_Pacman
 {
-    internal class Outro
+    class Outro
     {
         private Intro intro = new Intro();
-        public void OutroScene()
+
+        public Intro GetIntro()
         {
-            //Console.BackgroundColor = ConsoleColor.Blue;
+            return intro;
+        }
+
+        public void OutroScene(Intro intro)
+        {
             Console.ForegroundColor = ConsoleColor.White;
 
-
             int result = Player.Score;
+            Console.WriteLine();
+            Console.WriteLine("                                               [Total Score]");
+            Console.WriteLine();
 
-            Console.WriteLine("┌----------------------------------------┐");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine($"│            Total Score : {result,2}            │");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("└----------------------------------------┘");
+            Console.WriteLine("                                ┌----------------------------------------┐");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine($"                                │                   {result,2}                   │");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                └----------------------------------------┘");
 
-            Console.WriteLine("┌----------------------------------------┐");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("│              1.다시 시작               │");
-            Console.WriteLine("│              2.게임 종료               │");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("│                                        │");
-            Console.WriteLine("└----------------------------------------┘");
-            Console.WriteLine("\n\n\n\n");
-            Console.Write("번호 입력 >> ");
-            
+            Console.WriteLine("                                ┌----------------------------------------┐");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                │              1.다시 시작               │");
+            Console.WriteLine("                                │              2.게임 종료               │");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                │                                        │");
+            Console.WriteLine("                                └----------------------------------------┘");
+            Console.WriteLine("\n\n");
+            Console.Write("                                 번호 입력 >> ");
+
             try
             {
                 int key = Convert.ToInt32(Console.ReadLine());
@@ -47,7 +47,7 @@ namespace Project_Pacman
                     Console.WriteLine("새 게임을 시작합니다");
                     GameKill();
                     Console.Clear();
-                    intro.PrintScreen();
+                    Intro.PrintScreen();
                 }
                 else
                 {
@@ -56,13 +56,13 @@ namespace Project_Pacman
                     Environment.Exit(2);
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
                 Console.Clear();
-                OutroScene();
+                OutroScene(GetIntro());
             }
-            
+
         }
 
         public void GameKill()
